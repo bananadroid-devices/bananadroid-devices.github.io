e1962385ce03a0b7017cc892c372ef2aaceb13d6
Rizky Benggolo <travarilo@gmail.com>
2022-08-16 22:37:38 +0700

    Revamp website

--- js/scripts.js
+++ js/scripts.js
@@ -32,32 +32,46 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  effect: 'coverflow',
-  slidesPerView: 'auto',
-  autoplay: {
-    delay: 1700,
-    disableOnInteraction: false,
-  },
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 250,
-    modifier: 1,
-    slideShadows: false,
-  },
 
-  // If we need pagination
+/**
+ * Gallery Slider
+ */
+new Swiper('.gallery-slider', {
+  speed: 400,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
   pagination: {
     el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 7,
+      spaceBetween: 30
+    }
+  }
+});
+/**
+ * Initiate gallery lightbox 
+ */
+const galleryLightbox = GLightbox({
+  selector: '.gallery-lightbox'
 });
